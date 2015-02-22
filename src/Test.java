43ef95306a8a2972fb32b7223748ddf921ba4fb2
monahadibarhaghtalab <mona.hadibarhaghtalab@gmail.com>
2015-02-22 11:03:40 +0330

    div

--- src/Test.java
+++ src/Test.java
@@ -21,4 +21,20 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
+     private static void testDiv()
+    {
+        int a = 5;
+        int b = 6;
+        int expected = 11;
+        BasicMath bm = new BasicMath();
+        int result = bm.div(a, b);
+        if (result == expected)
+        {
+            System.out.println("Div: OK");
+        }
+        else
+        {
+            System.out.println("Div: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
 }
